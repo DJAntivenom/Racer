680b783140d95f4b48ad113bd2466c83074a712f
Dillon Elste <djantivenom@yahoo.com>
2018-11-12 10:32:34 +0100

    12.11.2018

--- src/ch/elste/racer/Player.java
+++ src/ch/elste/racer/Player.java
@@ -3,8 +3,8 @@ package ch.elste.racer;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.image.ImageObserver;
 
 import javax.swing.ImageIcon;
@@ -14,9 +14,10 @@ import ch.elste.racer.interfaces.Drawable;
 public class Player implements Drawable {
 	private Image sprite;
 	private double x, y;
+	private double dx;
+	private double speed;
 	private Dimension spriteSize, size;
 	private ImageObserver observer;
-	private ActionListener inputHandler;
 
 	/*
 	 * Kreiert einen neuen Player.
@@ -32,25 +33,20 @@ public class Player implements Drawable {
 		spriteSize = new Dimension(0, 0);
 		size = new Dimension(20, 20);
 
+		speed = 60;
+
 		loadImage();
-
-		inputHandler = new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-
-			}
-		};
 	}
 
 	/**
 	 * Lädt das Bild aus dem Speicher.
+	 * 
 	 * @throws InterruptedException
 	 */
 	public void loadImage() throws InterruptedException {
-		Thread loadingThread = new Thread(()->{
+		Thread loadingThread = new Thread(() -> {
 			sprite = new ImageIcon(getClass().getResource("data/Player.png")).getImage();
-			
+
 			spriteSize.setSize(sprite.getWidth(observer), sprite.getHeight(observer));
 		});
 		loadingThread.start();
@@ -60,13 +56,33 @@ public class Player implements Drawable {
 
 	@Override
 	public void draw(Graphics2D g2d, ImageObserver observer) {
-		g2d.drawImage(sprite, (int) Math.round(x - size.width / 2), (int) Math.round(y - size.height / 2),
-				size.width, size.height, observer);
+		g2d.drawImage(sprite, (int) Math.round(x - size.width / 2), (int) Math.round(y - size.height / 2), size.width,
+				size.height, observer);
 	}
-	
+
 	@Override
 	public void draw(Graphics2D g2d) {
+		move();
 		draw(g2d, observer);
+	}
+
+	public void move() {
+		if (dx > 5)
+			dx = 5;
+		if (x < RenderLogic.WIDTH)
+			x += dx;
+		else
+			x = 0;
+	}
+
+	public class KeyHandler extends KeyAdapter {
+		@Override
+		public void keyPressed(KeyEvent e) {
+			if (e.getKeyCode() == KeyEvent.VK_D)
+				dx += speed * RenderLogic.getDeltaTime();
+			else if(e.getKeyCode() == KeyEvent.VK_A)
+				dx -= speed * RenderLogic.getDeltaTime();
+		}
 	}
 
 	public double getX() {
--- src/ch/elste/racer/RenderLogic.java
+++ src/ch/elste/racer/RenderLogic.java
@@ -6,37 +6,52 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class RenderLogic implements Runnable {
-	private static final int WIDTH = 800, HEIGHT = 800;
-	
+	public static final int WIDTH = 800, HEIGHT = 800;
+
 	private JFrame window;
 	private JPanel gameScreen;
-	
+
 	private Player player;
-	
+
+	private static long frameStartTime, frameEndTime;
+	private static int deltaTime;
+
 	public RenderLogic() throws InterruptedException {
 		gameScreen = new JPanel();
-		
+
 		window = new JFrame("Racer");
 		window.setSize(WIDTH, HEIGHT);
 		window.setContentPane(gameScreen);
 		window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		window.setLocationRelativeTo(null);
 		window.setVisible(true);
-		
+
 		player = new Player();
 		player.setX(gameScreen.getWidth() / 2);
-		player.setY(gameScreen.getHeight() - player.getHeight()*2);
+		player.setY(gameScreen.getHeight() - player.getHeight() * 2);
 	}
 
 	@Override
 	public void run() {
-		while(window.isVisible()) {
+		while (window.isVisible()) {
 			draw((Graphics2D) gameScreen.getGraphics());
 		}
 	}
-	
+
 	private void draw(Graphics2D g2d) {
+		frameStartTime = System.nanoTime();
 		player.draw(g2d, gameScreen);
+
+		frameEndTime = System.nanoTime();
+		deltaTime = (int) (frameStartTime - frameEndTime * Math.pow(10, 6));
 	}
 
+	/**
+	 * Gibt die Zeit für die Berechnung des letzten Frames in Millisekunden zurück.
+	 * 
+	 * @return die Zeit für die Berechnung des letzten Frames in Millisekunden.
+	 */
+	public static int getDeltaTime() {
+		return deltaTime;
+	}
 }
